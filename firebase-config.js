7668cb21b859c3981c106218f28bb58b941f6dfc
billySamir <billybravo93@gmail.com>
2026-07-08 15:26:41 -0500

    login 2

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,21 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZSfCjwAm-Hy_1uk6xIxjm6KPShcZZITY",
+  authDomain: "busqueda-de-trabajo-b7f08.firebaseapp.com",
+  projectId: "busqueda-de-trabajo-b7f08",
+  storageBucket: "busqueda-de-trabajo-b7f08.firebasestorage.app",
+  messagingSenderId: "929331290926",
+  appId: "1:929331290926:web:33eacdf1ecbbb8439410bf",
+  measurementId: "G-XCVVWHH6GM"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
