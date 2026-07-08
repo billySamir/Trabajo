ecab38e782c1808f624003ac5be911f2ea5336b3
billySamir <billybravo93@gmail.com>
2026-07-08 15:47:31 -0500

    login 7

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAZsFCjwAm-Hy_1uK6xIxjm6KPShcZZITY", // Pégala aquí directamente para la prueba
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAZsFCjwAm-Hy_1uK6xIxjm6KPShcZZITY",
   authDomain: "busqueda-de-trabajo-b7f08.firebaseapp.com",
   projectId: "busqueda-de-trabajo-b7f08",
-  storageBucket: "busqueda-de-trabajo-b7f08.firebasestorage.app", // Usa el que te dé tu consola
+  storageBucket: "busqueda-de-trabajo-b7f08.appspot.com",
   messagingSenderId: "929331290926",
   appId: "1:929331290926:web:33eacdf1ecbbb8439410bf",
   measurementId: "G-XCVVWHH6GM"
