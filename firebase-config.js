44c87eafcb8c5e27dd589e3cb96c62195469f1bc
billySamir <billybravo93@gmail.com>
2026-07-08 15:32:01 -0500

    login 4

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,4 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-// Tu configuración web de Firebase
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyAZsFCjwAm-Hy_1uK6xIxjm6KPShcZZITY",
   authDomain: "busqueda-de-trabajo-b7f08.firebaseapp.com",
   projectId: "busqueda-de-trabajo-b7f08",
@@ -12,10 +7,3 @@ const firebaseConfig = {
   appId: "1:929331290926:web:33eacdf1ecbbb8439410bf",
   measurementId: "G-XCVVWHH6GM"
 };
-
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Exportar los servicios para usarlos en el resto de la aplicación
-export { app, analytics };
