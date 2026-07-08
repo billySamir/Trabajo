1bec618fa41238b2bac85e40241ff2a7d34b2f2a
billySamir <billybravo93@gmail.com>
2026-07-08 15:49:12 -0500

    login 8

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAZsFCjwAm-Hy_1uK6xIxjm6KPShcZZITY",
+  apiKey: "AIzaSyAZSfCjwAm-Hy_1uk6xIxjm6KPShcZZITY",
   authDomain: "busqueda-de-trabajo-b7f08.firebaseapp.com",
   projectId: "busqueda-de-trabajo-b7f08",
-  storageBucket: "busqueda-de-trabajo-b7f08.appspot.com",
+  storageBucket: "busqueda-de-trabajo-b7f08.firebasestorage.app",
   messagingSenderId: "929331290926",
   appId: "1:929331290926:web:33eacdf1ecbbb8439410bf",
   measurementId: "G-XCVVWHH6GM"
